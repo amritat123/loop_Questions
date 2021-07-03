17c8fc9f18bead4d49fef889aa8fc1d70b676709
amritat123 <83055726+amritat123@users.noreply.github.com>
2021-07-03 11:16:52 +0530

    Update series_question.py

--- series_question.py
+++ series_question.py
@@ -1,15 +1,15 @@
-# i=1
-# j=0
-# while(i<=10):
-# 	a=7*i+j
-# 	print(a)
-# 	i=i+1
-# 	j=j+1
+i=1
+j=0
+while(i<=10):
+	a=7*i+j
+	print(a)
+	i=i+1
+	j=j+1
 
-# #second way...
-# i=1
-# while i<=39:
-# 	if i %8==7:
-# 		print(i)
-# 	i+=1
+#second way...
+i=1
+while i<=39:
+	if i %8==7:
+		print(i)
+	i+=1
  

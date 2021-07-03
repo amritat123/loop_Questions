50b83dbfcc38526326b805ed1c0f50f286843ba4
amritat123 <83055726+amritat123@users.noreply.github.com>
2021-07-03 11:54:48 +0530

    Add files via upload

---
+++ series_of_number.py
@@ -0,0 +1,20 @@
+# i=1
+# j=0
+# while(i<=10):
+# 	a=7*i+j
+# 	print(a)
+# 	i=i+1
+# 	j=j+1
+
+# #second way...
+# # i=1
+# # while i<=39:
+# # 	if i %8==7:
+# # 		print(i)
+# # 	i+=1
+
+i=10
+while i<=300:
+    if i*10:
+        print(i, end=" ")
+    i+=10
